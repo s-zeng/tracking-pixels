ed1df442a655870c34bc506ededfbf2c112b568b
Simon Zeng <contact@simonzeng.com>
2023-03-05 20:47:43 -0500

    add .png

--- tracking.py
+++ tracking.py
@@ -12,6 +12,7 @@ pixel = "1x1.png"
 
 @app.route("/pixel/<path:name>", methods=["GET"])
 def track(name: str) -> Response:
+    name = name.split(".")[0]
     with open(names_file, "a") as f:
         f.write(f"{name} from {request.remote_addr}: {datetime.datetime.now()}\n")
     return send_file(pixel, mimetype="image/png")
